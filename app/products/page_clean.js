23bb8e1bee9fb350406ed54f608ca55d89f3d57f
Smart Kids Kuwait <info@smartkidskw.com>
2025-11-01 05:36:24 +0300

    chore(products): remove local /products index; use /collections/all

--- app/products/page_clean.js
+++
@@ -1,6 +0,0 @@
-﻿import { redirect } from 'next/navigation';
-
-// Redirect /products to /collections/all to match Shopify structure
-export default function ProductsRedirect() {
-  redirect('/collections/all');
-}
